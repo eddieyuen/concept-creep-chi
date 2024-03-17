4207830902eea6e42e2e086c1bfee4e6bf01cbb6
eddieyuen <eddieyuenkw@gmail.com>
2024-03-18 02:35:09 +0800

    Added output concept usages JSON file to semantic_breadth.py and fixed sampling problem

--- 1_code/semantic_breadth.py
+++ 1_code/semantic_breadth.py
@@ -5,20 +5,25 @@ from gensim.models import Word2Vec
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 import json
+import time
 
 # Initialize an empty list to store the results
 results = []
+
+# Initialize an empty dictionary to store the concept usages
+concept_usages_dict = {}
 
 # Read the concepts from the JSON file
 with open('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/concepts.json', 'r') as f:
     concepts = json.load(f)
 
 # Prompt the user for input regarding the naming of the output files
-file_number = input("Enter the filenumber (1st, 2nd, 3rd, 4th, etc.): ")
+file_number = input("Enter the filenumber (1, 2, 3, 4, etc.): ")
 
 # Loop through the years from 1979 to 2021
 for year in range(1979, 2022):
-    # Load the pretrained Word2Vec model
+
+    # Load the pretrained Word2Vec model and cleaned data for the year
     model_path = f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi_raw/models/pd_{year}.model'
     try:
         model = Word2Vec.load(model_path)
@@ -26,7 +31,6 @@ for year in range(1979, 2022):
         print(f"Model not found for {year}. Skipping...")
         continue
 
-    # Load the cleaned data for the year
     cleaned_data_path = f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi_raw/cleanedtext/cleaned_{year}.pkl'
     with open(cleaned_data_path, 'rb') as f:
         cleaned_data = pickle.load(f)
@@ -37,20 +41,27 @@ for year in range(1979, 2022):
 
         # Step 1: Randomly sample specific usages of a concept from the corpus
         token_occurrence = [token for sentence in cleaned_data for token in sentence if token == concept]
-        population_size = len(token_occurrence)
-        print(f'Population size for {concept} in {year}: {population_size}')
+        token_frequency = len(token_occurrence)
+        print(f'Frequency for {concept} in {year}: {token_frequency}')
 
         # Check the population size and adjust the sampling accordingly
-        if population_size < 20:
-            print(f"The population size for '{concept}' in {year} is smaller than 20. Skipping the year.")
+        if token_frequency < 20:
+            print(f"The frequency for '{concept}' in {year} is smaller than 20. Skipping the year.")
             # Append 'NA' values to the results list for this year and concept
             results.append({'Year': year, 'Concept': concept, 'Mean Cosine Similarity': 'NA', 'Semantic Breadth': 'NA'})
             continue
-        elif 20 <= population_size <= 50:
+        elif 20 <= token_frequency <= 50:
             concept_usages = [sentence for sentence in cleaned_data if concept in sentence]
         else:
-            random_token_occurrence = random.sample(token_occurrence, k=50)
-            concept_usages = [sentence for sentence in cleaned_data for token in sentence if token in random_token_occurrence]
+            concept_usages = [sentence for sentence in cleaned_data if concept in sentence]
+            if len(concept_usages) > 50: 
+                random.seed(int(time.time()))
+                random_concept_usages = random.sample(concept_usages, k=50)
+                concept_usages = random_concept_usages
+
+        # Store the concept usages in the dictionary
+        key = (year, concept)
+        concept_usages_dict[str(key)] = concept_usages
             
         # Step 2: Compute the context vectors for each specific usage of the concept
         if 'concept_usages' in locals():
@@ -88,7 +99,7 @@ for year in range(1979, 2022):
             semantic_breadth = 1 / mean_cosine_similarity
 
             # Append the results to the list
-            results.append({'Year': year, 'Concept': concept, 'Mean Cosine Similarity': mean_cosine_similarity, 'Semantic Breadth': semantic_breadth})
+            results.append({'Year': year, 'Concept': concept, 'Mean Cosine Similarity': mean_cosine_similarity, 'Semantic Breadth':semantic_breadth})
 
 # Convert the results list to a DataFrame
 results_df = pd.DataFrame(results)
@@ -99,7 +110,7 @@ output_path = f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/prepro
 # Save the DataFrame as a CSV file
 results_df.to_csv(output_path, index=False)
 
-print("Output saved to: ", output_path)
+print("Semantic breadth output saved to: ", output_path)
 
 # Transposing the CSV file output
 # Read the original CSV file
@@ -115,3 +126,9 @@ transposed_df.columns = [f"{col[1]}_{col[0].replace(' ', '_')}" for col in trans
 transposed_df.to_csv(f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/semantic_breadth/semantic_breadth_{file_number}_transposed.csv', index_label='Year')
 
 print("CSV file with the desired structure has been created.")
+
+# Save the concept usages dictionary as a JSON file
+concept_usages_output_path = f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/semantic_breadth/concept_usages_{file_number}.json'
+with open(concept_usages_output_path, 'w') as f:
+    json.dump(concept_usages_dict, f, indent=4)
+print('Concept usages JSON file saved.')
