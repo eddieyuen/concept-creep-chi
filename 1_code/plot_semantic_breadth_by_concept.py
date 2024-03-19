8ed1fe6c4fb88a4f7e48d1595c78e2348c7b08e8
eddieyuen <eddieyuenkw@gmail.com>
2024-03-19 19:39:10 +0800

    Used English translations for concepts for filenames and plots for semantic breadth output

--- 1_code/plot_semantic_breadth_by_concept.py
+++ 1_code/plot_semantic_breadth_by_concept.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 
 # Read the mean semantic breadth file
 mean_data = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/semantic_breadth_mean.csv',index_col='Year')
 
+# Read the concept translations from the dictionary
+with open('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/concepts.json') as f:
+    concept_translations = json.load(f)
+
 # Define the target concepts
 target_concepts = ['主权', '偏见', '分裂', '创伤', '恐怖主义', '欺凌', '精神病', '阴谋']
+# Reverse the key-value pairs in the concept_translations dictionary
+english_translations = {value[0]: key for key, value in concept_translations.items()}
 
 # Specify the output directory
 output_directory = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/3_output/semantic_breadth/'
 
 # Iterate over each target concept and create line plots
 for concept in target_concepts:
+    # Get the English translation for the current concept
+    english_translation = english_translations.get(concept, concept)
+
     # Set up the figure and axes
     plt.rcParams['font.family'] = ['Heiti TC']
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -32,21 +42,18 @@ for concept in target_concepts:
     # Set the x-axis range from 1979 to 2021
     ax.set_xlim(1979, 2021)
     
-    # Set the y-axis starting from 0
-    # ax.set_ylim(0)
-    
     # Set the x-axis and y-axis labels
     ax.set_xlabel('Year')
     ax.set_ylabel('Semantic Breadth')
     
     # Set the title of the plot
-    ax.set_title(f'Semantic Breadth of Concept: {concept}')
+    ax.set_title(f'Semantic Breadth of Concept: {english_translation} ({concept})')
     
     # Show the legend
     ax.legend()
     
     # Save the plot to the output directory
-    output_file = output_directory + f'{concept}_semantic_breadth_mean.png'
+    output_file = output_directory + f'{english_translation}_semantic_breadth.png'
     plt.savefig(output_file)
 
     # Close the plot
