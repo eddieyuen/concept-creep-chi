04f8b4876999821c6ee19c113c688346b2724e94
eddieyuen <eddieyuenkw@gmail.com>
2024-03-22 00:10:28 +0800

    Amended plots for MFD cultural salience

--- 1_code/plot_relativefreq_mfd_virvic.py
+++ 1_code/plot_relativefreq_mfd_virvic.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_mfd_virvic.csv')
+data_virvic = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_mfd_virvic.csv')
+data_combined = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_mfd.csv')
 
 foundations = ['Harm', 'Fairness', 'Ingroup', 'Authority', 'Purity']
 colors = ['red', 'blue', 'green', 'orange', 'purple']
@@ -9,12 +10,14 @@ colors = ['red', 'blue', 'green', 'orange', 'purple']
 for foundation, color in zip(foundations, colors):
     virtue_column = f'{foundation}_virtue'
     vice_column = f'{foundation}_vice'
-    plt.plot(data['Year'], data[virtue_column], color=color, label=f'{foundation} Virtue')
-    plt.plot(data['Year'], data[vice_column], color=color, linestyle='dashed', label=f'{foundation} Vice')
+    plt.plot(data_combined['Year'], data_combined[foundation], color=color, linestyle='solid', linewidth=2, label=foundation)
+    plt.plot(data_virvic['Year'], data_virvic[virtue_column], color=color, linestyle='dotted', linewidth=0.8, alpha=0.7, label=f'{foundation} Virtue')
+    plt.plot(data_virvic['Year'], data_virvic[vice_column], color=color, linestyle='dashed', linewidth=0.8, alpha=0.7, label=f'{foundation} Vice')
     
     plt.xlabel('Year')
     plt.ylabel('Relative Frequency')
     plt.title(f'Cultural Salience of the {foundation} Foundation')
     plt.legend()
+    plt.grid(True)
     plt.savefig(f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi/3_output/relativefreq/relativefreq_{foundation.lower()}_virvic.png')
     plt.close()
