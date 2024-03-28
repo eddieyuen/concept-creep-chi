165aa69aa335acd8079e3fd3922927f9a98aad8b
eddieyuen <eddieyuenkw@gmail.com>
2024-03-29 01:13:15 +0800

    Plot relative frequency of target concepts

--- 1_code/plot_relativefreq_concepts.py
+++ 1_code/plot_relativefreq_concepts.py
@@ -2,40 +2,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
-# Define the target concepts and their corresponding column names
-target_concepts = ['偏见', '欺凌', '精神病', '创伤', '分裂', '阴谋', '主权', '恐怖主义']
+# Load the concepts from the concepts.json file
+concepts_file = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/concepts.json'
+with open(concepts_file, 'r') as f:
+    concepts_data = json.load(f)
+    # Reverse the key-value pairs for English translations
+    concept_translations = {value[0]: key for key, value in concepts_data.items()}
 
-# Read the concept translations from the dictionary
-with open('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/concepts.json') as f:
-    concept_translations = json.load(f)
-# Reverse the key-value pairs in the concept_translations dictionary
-english_translations = {value[0]: key for key, value in concept_translations.items()}
+# Define the corresponding column names of the target concepts
+concepts_columns = [concept[0] + '_freq' for concept in concepts_data.values()]
 
 # Specify file path
-relative_frequency_file_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_concepts.csv'
-
+relativefreq_concepts_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_concepts.csv'
 # Read relative frequency data
-data = pd.read_csv(relative_frequency_file_path)
+data = pd.read_csv(relativefreq_concepts_path)
 years = data['Year']
 
 # Set up the figure and axes for the normal plot
 plt.rcParams['font.family'] = ['Heiti TC']
 fig, ax = plt.subplots(figsize=(10, 8))
-
 # Define a list of colors to use for the line plots
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 # Iterate over each target concept and create line plots
-for i, concept in enumerate(target_concepts):
+for i, concepts_column in enumerate(concepts_columns):
     # Get the English translation for the current concept
-    english_translation = english_translations.get(concept, concept)
-    # Find the column name that matches the target concept
-    matched_column = data.columns[data.columns.str.contains(concept)]
+    english_translation = concept_translations.get(concepts_column.split('_')[0])
     # Get the color for the current concept
     color = colors[i]
     # Plot relative frequency values for each concept with a unique color
-    relativefreq = data[matched_column]
-    ax.plot(years, relativefreq, label=f'{english_translation} ({concept})', color=color)
+    relativefreq = data[concepts_column]
+    ax.plot(years, relativefreq, label=f'{english_translation} ({concepts_column.split("_")[0]})', color=color)
 
 ax.set_ylim(bottom=0)
 ax.set_xlabel('Year')
