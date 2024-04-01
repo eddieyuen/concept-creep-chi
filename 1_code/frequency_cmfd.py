41a2059a180436dea39d874ea8218d2b5df8a76d
eddieyuen <eddieyuenkw@gmail.com>
2024-04-01 18:38:17 +0800

    Add frequency of CMFD

--- 1_code/frequency_cmfd.py
+++ 1_code/frequency_cmfd.py
@@ -3,22 +3,28 @@ import pickle
 
 # Load the CSV file
 cmfd_data = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/cmfd.csv')
+frequency_cmfd = pd.read_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/frequency_cmfd.csv')
 
-# Load the pickle file
-with open('/Users/kawaiyuen/nlpworkshop/concept-creep-chi_raw/cleanedtext/cleaned_1979.pkl', 'rb') as file:
-    cleaned_tokens = pickle.load(file)
+for year in range (2017, 2024):
 
-# Function to count frequency
-def count_frequency(tokens, target):
-    count = 0
-    for sentence in tokens:
-        for token in sentence:
-            if token == target:
-                count += 1
-    return count
+    # Load the pickle file
+    with open(f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi_raw/cleanedtext/cleaned_{year}.pkl', 'rb') as file:
+        cleaned_tokens = pickle.load(file)
 
-# Add frequency column to cmfd_data
-cmfd_data['frequency'] = cmfd_data['chinese'].apply(lambda x: count_frequency(cleaned_tokens, x))
+    # Function to count frequency
+    def count_frequency(tokens, target):
+        count = 0
+        for sentence in tokens:
+            for token in sentence:
+                if token == target:
+                    count += 1
+        return count
 
-# Save the updated data to a new CSV file
-cmfd_data.to_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/frequency_cmfd.csv', index=False)
+    # Add frequency column 
+    frequency_cmfd[f'{year}'] = cmfd_data['chinese'].apply(lambda x: count_frequency(cleaned_tokens, x))
+
+    # Save the updated data 
+    frequency_cmfd.to_csv('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/frequency_cmfd.csv', index=False)
+    print(f'Frequency data for {year} saved.')
+
+print('All frequency data computed and saved.')
