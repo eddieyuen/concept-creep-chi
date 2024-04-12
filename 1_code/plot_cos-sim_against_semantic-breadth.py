b40fda462400d766d8bfb4bbbf024cff1a84505d
eddieyuen <eddieyuenkw@gmail.com>
2024-04-12 16:59:11 +0800

    Fix error in plot labeling

--- 1_code/plot_cos-sim_against_semantic-breadth.py
+++ 1_code/plot_cos-sim_against_semantic-breadth.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+concept = input('Enter concept name:')
+concept_chi = input('Enter concept in Chinese:')
+
 # Read the Excel file
 df = pd.read_excel('/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/main-3-final.xlsx')
 
 # Specify the columns to be plotted
-left_y_axis_column = '恐怖主义_Semantic_Breadth'
-right_y_axis_columns = ['terrorism_care', 'terrorism_fair', 'terrorism_loya', 'terrorism_auth', 'terrorism_sanc']
+left_y_axis_column = f'{concept_chi}_Semantic_Breadth'
+right_y_axis_columns = [f'{concept}_care', f'{concept}_fair', f'{concept}_loya', f'{concept}_auth', f'{concept}_sanc']
 right_y_axis_labels = ['harm', 'fairness', 'ingroup', 'authority', 'purity']
 x_axis_column = 'Year'
 
@@ -14,7 +17,7 @@ x_axis_column = 'Year'
 fig, ax1 = plt.subplots()
 
 # Plot the left y-axis column
-ax1.plot(df[x_axis_column], df[left_y_axis_column], color='black', linewidth=1.2, label='prejudice')
+ax1.plot(df[x_axis_column], df[left_y_axis_column], color='black', linewidth=1.2, label=f'{concept}')
 ax1.set_xlabel(x_axis_column)
 ax1.set_ylabel('Semantic Breadth')
 ax1.legend(loc='lower left')
@@ -29,5 +32,5 @@ ax2.set_ylabel('Cosine Similarity')
 ax2.legend(loc='upper right')
 
 # Save the plot
-output_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/3_output/cos-sim/terrorism.png'
+output_path = f'/Users/kawaiyuen/nlpworkshop/concept-creep-chi/3_output/cos-sim/{concept}.png'
 plt.savefig(output_path, dpi=300)
