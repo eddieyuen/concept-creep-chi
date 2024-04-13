723e666e356aa9d34dc3fcf241e988a506c6dee5
eddieyuen <eddieyuenkw@gmail.com>
2024-04-14 03:25:00 +0800

    Fix contextualized representation in semantic breadth code and added four control words

---
+++ 1_code/frequency_control_words.py
@@ -0,0 +1,49 @@
+import pickle
+import json
+import csv
+
+# Path to the cleaned text pickle files
+cleanedtext_folder = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi_raw/cleanedtext/'
+
+# Path to the concepts.json file
+control_words_file = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/0_data/wordlist/control_words.json'
+
+# Path to the output CSV file
+output_csv = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/frequency_control_words.csv'
+
+def extract_token_frequency(token, year):
+    # Load the cleaned tokens from the pickle file
+    cleanedtext_path = cleanedtext_folder + f'cleaned_{year}.pkl'
+    with open(cleanedtext_path, 'rb') as f:
+        cleaned_tokens = pickle.load(f)
+
+    # Count the frequency of the token
+    token_frequency = sum(token == t for sentence in cleaned_tokens for t in sentence)
+
+    return token_frequency
+
+# Load the concepts from the concepts.json file
+with open(control_words_file, 'r') as f:
+    control_words_data = json.load(f)
+
+# Prepare the header and data for the CSV file
+header = ['Year'] + [concept[0] + '_freq' for concept in control_words_data.values()]  
+data = []
+
+# Extract the frequencies for each token and year
+for year in range(1979, 2024):
+    row = [year]
+    for token_key in control_words_data:
+        token = control_words_data[token_key][0]  # Get the first item from the list of tokens
+        frequency = extract_token_frequency(token, year)
+        row.append(frequency)
+    data.append(row)
+    print(f'Frequency data for {year} computed.')
+
+# Write the data to the output CSV file
+with open(output_csv, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(data)
+
+print("Frequency data has been successfully stored in the CSV file.")
--- 1_code/semantic_breadth_mean.py
+++ 1_code/semantic_breadth_mean.py
@@ -7,8 +7,8 @@ combined_df = pd.DataFrame()
 for file_number in range(1, 11):
     
     # Construct the file name using the suffix
-    file_name = f"semantic_breadth_{file_number}_transposed.csv"
-    file_path = f"/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/semantic_breadth_samples/{file_name}"
+    file_name = f"controls_sb_{file_number}_transposed.csv"
+    file_path = f"/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/preprocessed/semantic_breadth_control_words/{file_name}"
 
     # Read the transposed CSV file for the corresponding file number
     df = pd.read_csv(file_path, index_col='Year')
@@ -20,7 +20,7 @@ for file_number in range(1, 11):
 mean_df = combined_df.groupby('Year').mean()
 
 # Define the path to the output CSV file
-output_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/semantic_breadth_mean.csv'
+output_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/controls_semantic_breadth_mean.csv'
 
 # Save the mean values DataFrame as a CSV file
 mean_df.to_csv(output_path)
