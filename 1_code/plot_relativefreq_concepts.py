6ad31afcf7178751ff8170c038e7629860f40fde
eddieyuen <eddieyuenkw@gmail.com>
2024-05-06 22:47:24 +0800

    Add frequency plots

--- 1_code/plot_relativefreq_concepts.py
+++ 1_code/plot_relativefreq_concepts.py
@@ -10,12 +10,12 @@ with open(concepts_file, 'r') as f:
     concept_translations = {value[0]: key for key, value in concepts_data.items()}
 
 # Define the corresponding column names of the target concepts
-concepts_columns = [concept[0] + '_freq' for concept in concepts_data.values()]
+concepts_columns = [concept[0] + '_relativefreq' for concept in concepts_data.items()]
 
 # Specify file path
-relativefreq_concepts_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/tmp/relativefreq_concepts.csv'
+relativefreq_concepts_path = '/Users/kawaiyuen/nlpworkshop/concept-creep-chi/2_pipeline/MAIN-FULL_sb-win-size-5.xlsx'
 # Read relative frequency data
-data = pd.read_csv(relativefreq_concepts_path)
+data = pd.read_excel(relativefreq_concepts_path)
 years = data['Year']
 
 # Set up the figure and axes for the normal plot
@@ -26,13 +26,13 @@ colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 # Iterate over each target concept and create line plots
 for i, concepts_column in enumerate(concepts_columns):
-    # Get the English translation for the current concept
-    english_translation = concept_translations.get(concepts_column.split('_')[0])
+    # Get the Chinese translation for the current concept
+    chinese_concept = concepts_data.get(concepts_column.split("_")[0])[0]
     # Get the color for the current concept
     color = colors[i]
     # Plot relative frequency values for each concept with a unique color
     relativefreq = data[concepts_column]
-    ax.plot(years, relativefreq, label=f'{english_translation} ({concepts_column.split("_")[0]})', color=color)
+    ax.plot(years, relativefreq, label=f'{concepts_column.split("_")[0]} ({chinese_concept})', color=color)
 
 ax.set_ylim(bottom=0)
 ax.set_xlabel('Year')
@@ -54,16 +54,16 @@ fig, ax = plt.subplots(figsize=(10, 8))
 
 # Iterate over each target concept and create line plots
 for i, concepts_column in enumerate(concepts_columns):
-    # Get the English translation for the current concept
-    english_translation = concept_translations.get(concepts_column.split('_')[0])
+    # Get the Chinese translation for the current concept
+    chinese_concept = concepts_data.get(concepts_column.split("_")[0])[0]
     # Get the color for the current concept
     color = colors[i]
     # Plot relative frequency values for each concept with a unique color
     relativefreq = data[concepts_column]
-    ax.plot(years, relativefreq, alpha=0.6, linestyle='dashed',linewidth=0.6, color=color)
+    ax.plot(years, relativefreq, alpha=0.7, linestyle='dashed',linewidth=0.6, color=color)
     # Calculate and plot moving average with window=5
     moving_average = data[concepts_column].rolling(window=5, min_periods=1).mean()
-    ax.plot(years, moving_average, label=f'{english_translation} ({concepts_column.split("_")[0]})', linewidth=2.4, color=color)
+    ax.plot(years, moving_average, label=f'{concepts_column.split("_")[0]} ({chinese_concept})', linewidth=2, color=color)
 
 ax.set_ylim(bottom=0)
 ax.set_xlabel('Year')
